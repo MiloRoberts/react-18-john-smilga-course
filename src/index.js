cabf5ac59b100cfc47f439624e623661b43dbf04
milo <mroberts18@bristolcc.edu>
2023-09-19 04:49:49 -0400

    Completed through lesson 32

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+const firstBook = {
+    author: 'Arthur C. Brooks',
+    title: 'Build the Life You Want',
+    img: "https://images-na.ssl-images-amazon.com/images/I/81+MJor-K6L._AC_UL600_SR600,400_.jpg"
+};
+const secondBook = {
+    author: 'Stephen King',
+    title: 'Holly',
+    img: "https://images-na.ssl-images-amazon.com/images/I/815oQ6G6HDL._AC_UL600_SR600,400_.jpg"
+};
+
+const BookList = () => {
+    return <section className="booklist">
+        <Book author={firstBook.author} title={firstBook.title} img={firstBook.img}>
+        </Book>
+        <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
+    </section >;
+};
+
+const Book = (props) => {
+    const { img, title, author } = props;
+    console.log(props);
+    return <article className="book">
+        <img src={img} alt={title} />
+        <h2>{title}</h2>
+        <h4>{author}</h4>
+    </article>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<BookList />);

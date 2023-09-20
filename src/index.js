b906795a28b8c8fcd770c7b1ad41d39d8759e8ea
milo <mroberts18@bristolcc.edu>
2023-09-20 14:58:39 -0400

    Completed through lesson 40

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const books = [
 const BookList = () => {
     return (
         <section className="booklist">
+            <EventExamples />
             {books.map((book) => {
                 return <Book {...book} key={book.id} />;
             })}
@@ -26,8 +27,22 @@ const BookList = () => {
     );
 };
 
+const EventExamples = () => {
+    return <section>
+        <form>
+            <h2>Typical Form</h2>
+            <input type="text" name="product" onChange={(event) => { console.log(event.target.value); }} style={{ margin: '1rem 0' }} />
+            <button type="submit">submit</button>
+            <div>
+                <button onClick={() => { console.log('click me'); }} type="button">click me</button>
+            </div>
+        </form>
+    </section>;
+};
+
 const Book = (props) => {
     const { img, title, author } = props;
+    // console.log(props);
     return <article className="book">
         <img src={img} alt={title} />
         <h2>{title}</h2>

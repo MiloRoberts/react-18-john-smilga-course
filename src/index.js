f83bb9bef12940f3a3aa2c9b7290bf2f76fe1265
milo <mroberts18@bristolcc.edu>
2023-09-25 22:48:30 -0400

    Completed through lesson 51

--- src/index.js
+++ src/index.js
@@ -2,44 +2,20 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-const books = [
-    {
-        author: 'Arthur C. Brooks',
-        title: 'Build the Life You Want',
-        img: "https://images-na.ssl-images-amazon.com/images/I/81+MJor-K6L._AC_UL600_SR600,400_.jpg",
-        id: 1
-    }, {
-        author: 'Stephen King',
-        title: 'Holly',
-        img: "https://images-na.ssl-images-amazon.com/images/I/815oQ6G6HDL._AC_UL600_SR600,400_.jpg",
-        id: 2
-    }
-];
+import { books } from './books.js';
+import Book from './Book.js';
 
 const BookList = () => {
-    const getBook = (id) => {
-        const book = books.find((book) => { return book.id === id; });
-        console.log(book);
-    };
     return (
-        <section className="booklist">
-            {books.map((book) => {
-                return <Book {...book} key={book.id} getBook={getBook} />;
-            })}
-        </section>
+        <React.Fragment>
+            <h1>amazon best sellers</h1>
+            <section className="booklist">
+                {books.map((book, index) => {
+                    return <Book {...book} key={book.id} number={index} />;
+                })}
+            </section>
+        </React.Fragment>
     );
-};
-
-const Book = (props) => {
-    const { img, title, author, id, getBook } = props;
-    // console.log(props);
-    // const getSingleBook = () => { getBook(id); };
-    return <article className="book">
-        <img src={img} alt={title} />
-        <h2>{title}</h2>
-        <button onClick={() => { getBook(id); }}>click me</button>
-        <h4>{author}</h4>
-    </article>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

6a5b13c4d48a7bb19631857ba4ffe1d6139a0a59
milo <mroberts18@bristolcc.edu>
2024-01-19 07:43:35 -0500

    Completed through lesson 25

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
-  return <h2>My First Component</h2>;
-}
+import './index.css';
+
+const Booklist = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return <article className="book">
+    <Image />
+    <Title />
+    <Author />
+  </article>;
+};
+
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg" alt="" />;
+const Title = () => <h2>Atomic Habits</h2>;
+const Author = () => {
+  return <h4>James Clear</h4>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting />);
+root.render(<Booklist />);

627f24780adb81614271311cc3614720a65dd295
milo <mroberts18@bristolcc.edu>
2024-01-25 08:48:34 -0500

    Completed through lesson 58

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     console.log('hello');
+//     return (
+//       <h1>backroads app</h1>
+//     );
+//   }
+// }
+
+import React from 'react';
+import Navbar from './components/Navbar.js';
+import Hero from './components/Hero.js';
+import Services from './components/Services.js';
+import Tours from './components/Tours.js';
+import Footer from './components/Footer.js';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Services></Services>
+      <Tours></Tours>
+      <Footer></Footer>
+    </React.Fragment >
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { books } from "./books.js";
-import Book from "./Book";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
-const Booklist = () => {
-  return (
-    <section className="booklist">
-      {books.map((book) => {
-        return (
-          <Book {...book} key={book.id} />
-        );
-      })}
-    </section>
-  );
-};
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<Booklist />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
